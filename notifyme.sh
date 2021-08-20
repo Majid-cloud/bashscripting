0cacb219f1076ec997290b8acc742df619e89781
majid-cloud <majid.asad04@gmail.com>
2021-08-20 18:21:26 +0500

    second part almost

--- notifyme.sh
+++ notifyme.sh
@@ -14,7 +14,30 @@ if [ $TIMEDIFF -gt $OLDTIME ]; then
    echo "not changed"
 else
    echo "file has changed changed"
-   echo "this is email form ubnut terminal, there is change in the content of the file" | mail -s "Changes Alert!!!!!" xflow@localhost
+   echo "this is email form ubnut terminal, there is change in the content of the file" | 
+   mail -s "Changes Alert!!!!!" xflow@localhost
 
 fi
 
+
+
+
+#part 2 of the task
+#checking if the webserver is installed or not
+webser=expr sudo systemctl status apache2
+loc=which apache2
+if [ loc != "" ]
+then
+    echo "apache is installed"
+else
+    echo "apache is not installed"
+   # now lets install apache2 if not installed
+    #lets update
+    sudo apt update
+    #install apache2
+    sudo apt install apache2
+    #allow apache on firewall
+    sudo ufw allow 'Apache'
+    #after installation check the status
+    sudo systemctl status apache2
+fi
